59100c10d57e5f123ed1e4e75d420f10cf20067a
supermanshuvo <shuvothesuperman@gmail.com>
2021-06-30 23:03:21 +0600

    Get the value and create text node and appendChild with the text node

--- myjs.js
+++ myjs.js
@@ -1,6 +1,8 @@
 function addTask(){
     let inputValue = document.getElementById('todo').value;
     let li = document.createElement('li');
+    let t = document.createTextNode(inputValue);
+    li.appendChild(t);
     if(inputValue == ''){
         alert('Kindly Input something you need to do!');
     }else{
