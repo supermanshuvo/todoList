088558881426f40aa9a140d4c68eaf39896ee7a2
supermanshuvo <shuvothesuperman@gmail.com>
2021-07-01 01:21:12 +0600

    Testing in console final done

--- myjs.js
+++ myjs.js
@@ -1,12 +1,28 @@
-let close = document.getElementsByClassName('close');
+let close = document.getElementsByClassName('delete');
 for(let i=0;i<close.length;i++){    
     close[i].onclick = function(){        
         let div = this.parentElement;
-        console.log(div.value);
-        div.style.display = 'none';
+        div.style.display= 'none';
     }
 }
 
+let done = document.getElementsByClassName('done');
+for(let i=0;i<done.length;i++){    
+    done[i].onclick = function(){        
+        let div = this.parentElement;
+        div.style.textDecoration= 'line-through';
+    }
+}
+
+/* function getEventTarget(e){
+    e = e || window.event;
+    return e.target || e.srcElement;
+}
+let selectItem = document.getElementById('paddingWorkList');
+selectItem.onclick = function(event){
+    let target = getEventTarget(event);
+    console.log(target.innerHTML);
+} */
 /* let selectItem = document.querySelector('ul');
 selectItem.addEventListener('click',function(e){
     if(e.target.tagName === 'LI'){
@@ -27,15 +43,26 @@ function addTask(){
         document.getElementById('paddingWorkList').appendChild(li);
     }
     document.getElementById('todo').value = "";
+    let doneText = document.createTextNode('Done');
+    let doneButton = document.createElement('BUTTON');
+    doneButton.className = 'done';
+    doneButton.appendChild(doneText);
+    li.appendChild(doneButton);
+    for(let j=0;j<done.length;j++){
+        done[j].onclick=function(){
+            let div = this.parentElement;
+            div.style.textDecoration= 'line-through';
+        }
+    }
     let button = document.createElement('BUTTON');
-    let txt = document.createTextNode('Delete');
-    button.className = 'close';
-    button.appendChild(txt);
+    let deleteText = document.createTextNode('Delete');
+    button.className = 'delete';
+    button.appendChild(deleteText);
     li.appendChild(button);
     for(let i=0;i<close.length;i++){
         close[i].onclick=function(){
             let div = this.parentElement;
-            div.style.display = 'none';
+            div.style.display= 'none';
         }
     }
 }
