8694da22c06dec59c55084d645e5c25261db2db6
supermanshuvo <shuvothesuperman@gmail.com>
2021-06-30 23:14:53 +0600

    Create element to display none

--- myjs.js
+++ myjs.js
@@ -9,4 +9,15 @@ function addTask(){
         console.log(inputValue);
     }
     document.getElementById('todo').value = "";
+    let span = document.createElement('SPAN');
+    let txt = document.createTextNode('\u00D7');
+    span.className = 'close';
+    span.appendChild(txt);
+    li.appendChild(span);
+    for(var i=0;i<close.length;i++){
+        close[i].onclick=function(){
+            let div = this.parentElement;
+            div.style.display = 'none';
+        }
+    }
 }
