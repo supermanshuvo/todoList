b3f89249e5a43c49aaf7d1a2cf5e988ec5369068
supermanshuvo <shuvothesuperman@gmail.com>
2021-07-03 15:26:30 +0600

    localstorage push done

--- public/myjs.js
+++ public/myjs.js
@@ -1,9 +1,13 @@
+let storage = localStorage.getItem('task');
+storage = storage ? storage.split(',') : [];
 function addTask(){
     let todoInput = document.getElementById('todo').value;
     let trimInput = todoInput.trim();
     if(trimInput=== '' || trimInput == ' '){
         alert('Kindly Enter something');
     }else{
-        // console.log(trimInput);
+        storage.push(trimInput);
+        localStorage.setItem('task', JSON.stringify(storage));
+        // console.log(JSON.parse(localStorage.getItem('task')));
     }
 }
