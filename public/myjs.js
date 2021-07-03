331ec7a49a3bf6a0acf538efe277810ee8d644db
supermanshuvo <shuvothesuperman@gmail.com>
2021-07-03 17:36:01 +0600

    show data and change the value

--- public/myjs.js
+++ public/myjs.js
@@ -28,6 +28,21 @@ function showTask(){
         taskObj = JSON.parse(storage);
     }
     let data = '';
-    
+    let workList = document.getElementById('paddingWorkList');
+    taskObj.forEach((item,index) => {
+        if(item.completeStatus==true){
+            taskCompleteValue = `<td class="completed">${item.task_name}</td>`;
+        }else{
+            taskCompleteValue = `<td>${item.task_name}</td>`;
+        }
+        data+=`<tr>
+        <th scope="row">${index+1}</th>
+        ${taskCompleteValue}
+        <td><button type="button" onclick="edittask(${index})" >Edit</button></td>
+        <td><button type="button" id=${index}>Complete</button></td>
+        <td><button type="button" onclick="deleteitem(${index})">Delete</button></td>
+    </tr>`;
+    });
+    workList.innerHTML = data;
     // console.log(JSON.parse(localStorage.getItem('task')));
 }
